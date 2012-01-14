bcd7ecb848947d1af0575238c7228b93dc9e2a2a
Avram Lyon <ajlyon@gmail.com>
2012-01-13 17:41:10 -0800

    Add build file, like Scaffold

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+find . -name '.hg' -prune -o \
+       -name '.hgtags' -prune -o \
+       -name '.svn' -prune -o \
+       -name '.git' -prune -o \
+       -name '.hgignore' -prune -o \
+       -name '.xpi' -prune -o \
+       -name 'fw-update.sh' -prune -o \
+       -name 'build.sh' -prune -o \
+       -name '*~' -prune -o \
+       -print | xargs zip zotfile.xpi
